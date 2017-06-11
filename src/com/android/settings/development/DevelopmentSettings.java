6d0d0715b15ba8899b44a2ea9e095944921d3bd9
Daniel Micay <danielmicay@gmail.com>
2017-06-11 17:50:31 -0400

    remove unsupported FBE conversion option
    Change-Id: Iaf74a368088519075608c53f2b30e68fec8acc9c

--- src/com/android/settings/development/DevelopmentSettings.java
+++ src/com/android/settings/development/DevelopmentSettings.java
@@ -237,8 +237,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
 
     private static final String TERMINAL_APP_PACKAGE = "com.android.terminal";
 
-    private static final String KEY_CONVERT_FBE = "convert_to_file_encryption";
-
     private static final int RESULT_DEBUG_APP = 1000;
     private static final int RESULT_MOCK_LOCATION_APP = 1001;
 
@@ -551,22 +549,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         if (hdcpChecking != null) {
             mAllPrefs.add(hdcpChecking);
             removePreferenceForProduction(hdcpChecking);
-        }
-
-        Preference convertFbePreference = findPreference(KEY_CONVERT_FBE);
-
-        try {
-            IBinder service = ServiceManager.getService("mount");
-            IStorageManager storageManager = IStorageManager.Stub.asInterface(service);
-            if (!storageManager.isConvertibleToFBE()) {
-                removePreference(KEY_CONVERT_FBE);
-            } else if ("file".equals(SystemProperties.get("ro.crypto.type", "none"))) {
-                convertFbePreference.setEnabled(false);
-                convertFbePreference.setSummary(getResources()
-                        .getString(R.string.convert_to_file_encryption_done));
-            }
-        } catch (RemoteException e) {
-            removePreference(KEY_CONVERT_FBE);
         }
 
         mColorModePreference = (ColorModePreference) findPreference(KEY_COLOR_MODE);
