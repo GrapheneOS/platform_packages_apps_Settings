2e06a66c5fc6a7ad6884a2be9902a3e1563031b6
Daniel Micay <danielmicay@gmail.com>
2017-06-16 00:22:43 -0400

    add device info field for anti-theft protection
    Change-Id: Ic5b292b3454c6e0812612628b9f69591c339b577

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -26,6 +26,7 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.deviceinfo.AdditionalSystemUpdatePreferenceController;
+import com.android.settings.deviceinfo.AntiTheftProtectionStatusPreferenceController;
 import com.android.settings.deviceinfo.BasebandVersionPreferenceController;
 import com.android.settings.deviceinfo.BootloaderVersionPreferenceController;
 import com.android.settings.deviceinfo.BuildNumberPreferenceController;
@@ -122,6 +123,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(
                 new BuildNumberPreferenceController(context, activity, fragment, lifecycle));
         controllers.add(new AdditionalSystemUpdatePreferenceController(context));
+        controllers.add(new AntiTheftProtectionStatusPreferenceController(context));
         controllers.add(new ManualPreferenceController(context));
         controllers.add(new FeedbackPreferenceController(fragment, context));
         controllers.add(new KernelVersionPreferenceController(context));
