2d854493bad6b383874c8f388c1d6e465d948a0d
Daniel Micay <danielmicay@gmail.com>
2017-09-08 12:41:27 -0400

    do not allow disabling the main keyboard
    It's not obvious that disabling it after installing another keyboard is
    a very bad idea. Other keyboards rarely support Direct Boot and won't
    work for entering the password, forcing recovery by plugging in a
    physical keyboard. Even if they do support Direct Boot it's a bad idea
    to rely on third party apps for basic functionality to keep working.
    
    Change-Id: I7a79f6c9412eb87231e00b2fbed0684a0fede767

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -238,7 +238,8 @@ public class InstalledAppDetails extends AppInfoBase
         // system cert and any launcher app in the system.
         if (mHomePackages.contains(mAppEntry.info.packageName)
                 || Utils.isSystemPackage(getContext().getResources(), mPm, mPackageInfo)
-                || mAppEntry.info.packageName.equals("org.chromium.chrome")) {
+                || mAppEntry.info.packageName.equals("org.chromium.chrome")
+                || mAppEntry.info.packageName.equals("com.android.inputmethod.latin")) {
             // Disable button for core system applications.
             button.setText(R.string.disable_text);
         } else if (mAppEntry.info.enabled && !isDisabledUntilUsed()) {
