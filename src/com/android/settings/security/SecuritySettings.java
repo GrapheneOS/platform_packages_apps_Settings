56f740d35c4f8f692ca8cf73b6cfdd29e57f7d9d
lsfxz <lsfxz@users.noreply.github.com>
2019-12-08 14:58:24 -0800

    expose deny_new_usb settings
    Based on original patch by Daniel Micay <danielmicay@gmail.com>.
    
    (ported from 10: dd79b1ee87a10d7c8cba91207bc12a1932ad43a5)

--- src/com/android/settings/security/SecuritySettings.java
+++ src/com/android/settings/security/SecuritySettings.java
@@ -115,6 +115,7 @@ public class SecuritySettings extends DashboardFragment {
         securityPreferenceControllers.add(new FingerprintStatusPreferenceController(context));
         securityPreferenceControllers.add(new ChangeScreenLockPreferenceController(context, host));
         securityPreferenceControllers.add(new PinScramblePreferenceController(context));
+        securityPreferenceControllers.add(new DenyNewUsbPreferenceController(context));
         controllers.add(new PreferenceCategoryController(context, SECURITY_CATEGORY)
                 .setChildren(securityPreferenceControllers));
         controllers.addAll(securityPreferenceControllers);
