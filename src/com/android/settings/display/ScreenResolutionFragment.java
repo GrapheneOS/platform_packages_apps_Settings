2774431f576128517c2323db41cd5c804e5c7a41
r3g_5z <june@girlboss.ceo>
2023-03-15 21:11:12 -0400

    remove missing display resolution lottie animation
    AOSP does not provide these similar to the missing quickly open camera
    lottie animation
    
    Signed-off-by: r3g_5z <june@girlboss.ceo>

--- src/com/android/settings/display/ScreenResolutionFragment.java
+++ src/com/android/settings/display/ScreenResolutionFragment.java
@@ -101,7 +101,6 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
 
     @Override
     protected void addStaticPreferences(PreferenceScreen screen) {
-        updateIllustrationImage(mImagePreference);
         screen.addPreference(mImagePreference);
 
         final FooterPreference footerPreference = new FooterPreference(screen.getContext());
@@ -223,7 +222,6 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
         }
 
         setDisplayMode(width);
-        updateIllustrationImage(mImagePreference);
 
         return true;
     }
