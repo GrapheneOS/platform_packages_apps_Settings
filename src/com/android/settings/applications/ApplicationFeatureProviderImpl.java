c13c6c540dde410a5b595d343b881c330cff40dd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-27 16:32:39 +0300

    do not allow disabling EuiccSupportPixel

--- src/com/android/settings/applications/ApplicationFeatureProviderImpl.java
+++ src/com/android/settings/applications/ApplicationFeatureProviderImpl.java
@@ -40,6 +40,7 @@ import android.util.Log;
 import androidx.annotation.VisibleForTesting;
 
 import com.android.internal.telephony.SmsApplication;
+import com.android.internal.util.GoogleEuicc;
 import com.android.settings.R;
 
 import java.util.ArrayList;
@@ -189,6 +190,8 @@ public class ApplicationFeatureProviderImpl implements ApplicationFeatureProvide
         keepEnabledPackages.add("app.grapheneos.camera");
         keepEnabledPackages.add(com.android.internal.gmscompat.GmsCompatApp.PKG_NAME);
         keepEnabledPackages.add(com.android.internal.gmscompat.GmsCompatApp.PKG_NAME + ".config");
+        // handles firmware updates of embedded secure element that is used for eSIM, NFC, Felica etc
+        keepEnabledPackages.add(GoogleEuicc.EUICC_SUPPORT_PIXEL_PKG_NAME);
 
         return keepEnabledPackages;
     }
