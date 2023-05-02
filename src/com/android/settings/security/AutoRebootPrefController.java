e1af2b92af4972944cd620a56e021c5d2d6133ee
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-02 21:49:05 +0300

    switch auto reboot setting to RadioButtonPickerFragment

--- src/com/android/settings/security/AutoRebootPrefController.java
+++ src/com/android/settings/security/AutoRebootPrefController.java
@@ -3,8 +3,11 @@ package com.android.settings.security;
 import android.content.Context;
 import android.ext.settings.ExtSettings;
 
+import androidx.preference.PreferenceScreen;
+
 import com.android.settings.R;
 import com.android.settings.ext.IntSettingPrefController;
+import com.android.settings.ext.RadioButtonPickerFragment2;
 
 import static java.util.concurrent.TimeUnit.DAYS;
 import static java.util.concurrent.TimeUnit.HOURS;
@@ -17,7 +20,14 @@ public class AutoRebootPrefController extends IntSettingPrefController {
     }
 
     @Override
+    public void addPrefsBeforeList(RadioButtonPickerFragment2 fragment, PreferenceScreen screen) {
+        addFooterPreference(screen, R.string.auto_reboot_footer,
+                "https://grapheneos.org/features#auto-reboot");
+    }
+
+    @Override
     protected void getEntries(Entries entries) {
+        entries.add(3, DAYS); // default
         entries.add(R.string.switch_off_text, 0);
         entries.add(10, MINUTES);
         entries.add(30, MINUTES);
@@ -29,6 +39,5 @@ public class AutoRebootPrefController extends IntSettingPrefController {
         entries.add(1, DAYS);
         entries.add(36, HOURS);
         entries.add(2, DAYS);
-        entries.add(3, DAYS);
     }
 }
