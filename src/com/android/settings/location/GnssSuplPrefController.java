cc94a386ad8c8adb5756e2dc0d5a4ccc89087006
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-03 08:56:28 +0300

    switch GNSS SUPL setting to RadioButtonPickerFragment

--- src/com/android/settings/location/GnssSuplPrefController.java
+++ src/com/android/settings/location/GnssSuplPrefController.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.ext.settings.ExtSettings;
 import android.ext.settings.GnssConstants;
 
+import androidx.preference.PreferenceScreen;
+
 import com.android.settings.R;
 import com.android.settings.ext.IntSettingPrefController;
+import com.android.settings.ext.RadioButtonPickerFragment2;
 
 public class GnssSuplPrefController extends IntSettingPrefController {
 
@@ -14,9 +17,14 @@ public class GnssSuplPrefController extends IntSettingPrefController {
     }
 
     @Override
+    public void addPrefsAfterList(RadioButtonPickerFragment2 fragment, PreferenceScreen screen) {
+        addFooterPreference(screen, R.string.pref_gnss_supl_footer);
+    }
+
+    @Override
     protected void getEntries(Entries entries) {
         entries.add(R.string.supl_enabled_grapheneos_proxy, GnssConstants.SUPL_SERVER_GRAPHENEOS_PROXY);
         entries.add(R.string.supl_enabled_standard_server, GnssConstants.SUPL_SERVER_STANDARD);
-        entries.add(R.string.supl_disabled, GnssConstants.SUPL_DISABLED);
+        entries.add(R.string.supl_disabled, R.string.supl_disabled_summary, GnssConstants.SUPL_DISABLED);
     }
 }
