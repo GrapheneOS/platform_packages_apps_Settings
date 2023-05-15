c8f7f47ca446791fdb19cf97d0c272f3fc381cf6
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-15 15:41:55 +0300

    adjust Storage Scopes "App info" screen link

--- src/com/android/settings/applications/appinfo/AppStorageScopesPreferenceController.java
+++ src/com/android/settings/applications/appinfo/AppStorageScopesPreferenceController.java
@@ -3,36 +3,22 @@ package com.android.settings.applications.appinfo;
 import android.app.StorageScope;
 import android.content.Context;
 import android.content.pm.GosPackageState;
-import android.text.TextUtils;
 
-import androidx.preference.Preference;
+import com.android.settings.ext.AppInfoPreferenceControllerBase2;
 
-public class AppStorageScopesPreferenceController extends AppInfoPreferenceControllerBase {
+public class AppStorageScopesPreferenceController extends AppInfoPreferenceControllerBase2 {
     public AppStorageScopesPreferenceController(Context context, String key) {
         super(context, key);
     }
 
     @Override
     public int getAvailabilityStatus() {
-        GosPackageState ps = GosPackageState.get(getPackageName());
-        if (ps != null && ps.hasFlag(GosPackageState.FLAG_STORAGE_SCOPES_ENABLED)) {
-            return AVAILABLE;
-        }
-
-        return CONDITIONALLY_UNAVAILABLE;
+        return hasGosPackageStateFlags(GosPackageState.FLAG_STORAGE_SCOPES_ENABLED) ?
+                AVAILABLE : CONDITIONALLY_UNAVAILABLE;
     }
 
     @Override
-    public boolean handlePreferenceTreeClick(Preference preference) {
-        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
-            return false;
-        }
-
-        mContext.startActivity(StorageScope.createConfigActivityIntent(getPackageName()));
-        return true;
-    }
-
-    private String getPackageName() {
-        return mParent.getPackageInfo().packageName;
+    public void onPreferenceClick(String packageName) {
+        mContext.startActivity(StorageScope.createConfigActivityIntent(packageName));
     }
 }
