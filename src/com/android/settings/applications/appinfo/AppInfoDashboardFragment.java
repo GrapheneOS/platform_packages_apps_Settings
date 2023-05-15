b9869a748cd2f463bfe5328efa4eefa89c6690d5
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-15 15:42:11 +0300

    add Contact Scopes link to the "App info" screen

--- src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
+++ src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
@@ -179,6 +179,7 @@ public class AppInfoDashboardFragment extends DashboardFragment
         use(AppStoragePreferenceController.class).setParentFragment(this);
         use(AppVersionPreferenceController.class).setParentFragment(this);
         use(AppStorageScopesPreferenceController.class).setParentFragment(this);
+        use(AppContactScopesPreferenceController.class).setParentFragment(this);
         use(InstantAppDomainsPreferenceController.class).setParentFragment(this);
 
         final HibernationSwitchPreferenceController appHibernationSettings =
