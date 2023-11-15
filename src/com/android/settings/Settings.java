5ee19abb90141be40ec544824ee88c4cc9fef976
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-15 13:51:07 +0200

    add per-app setting for native debugging

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -516,4 +516,6 @@ public class Settings extends SettingsActivity {
      * Activity for OneHandedSettings
      */
     public static class OneHandedSettingsActivity extends SettingsActivity { /* empty */ }
+
+    public static class AppNativeDebuggingActivity extends SettingsActivity {}
 }
