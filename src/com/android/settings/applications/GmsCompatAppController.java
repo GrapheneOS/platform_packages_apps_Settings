4d80ec1d4ea390707f6b73b10c0b8041f0513f09
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-23 11:15:54 +0200

    fixup! add link to Sandboxed Google Play settings
    GmsCompat.isGmsApp() was renamed to GmsCompat.isEnabledFor().

--- src/com/android/settings/applications/GmsCompatAppController.java
+++ src/com/android/settings/applications/GmsCompatAppController.java
@@ -1,23 +1,8 @@
-/*
- * Copyright (C) 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 package com.android.settings.applications;
 
 import android.app.compat.gms.GmsCompat;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.UserHandle;
 import android.text.TextUtils;
 
@@ -28,11 +13,9 @@ import com.android.internal.gmscompat.GmsInfo;
 import com.android.settings.core.BasePreferenceController;
 
 public class GmsCompatAppController extends BasePreferenceController {
-    private final Context context;
 
     public GmsCompatAppController(Context context, String key) {
         super(context, key);
-        this.context = context;
     }
 
     @Override
@@ -42,7 +25,7 @@ public class GmsCompatAppController extends BasePreferenceController {
                 workProfile.getIdentifier() :
                 UserHandle.myUserId();
 
-        return GmsCompat.isGmsApp(GmsInfo.PACKAGE_GMS_CORE, userId) ?
+        return GmsCompat.isEnabledFor(GmsInfo.PACKAGE_GMS_CORE, userId) ?
                 AVAILABLE : DISABLED_FOR_USER;
     }
 
@@ -56,9 +39,9 @@ public class GmsCompatAppController extends BasePreferenceController {
 
         UserHandle workProfile = getWorkProfileUser();
         if (workProfile != null) {
-            context.startActivityAsUser(intent, workProfile);
+            mContext.startActivityAsUser(intent, workProfile);
         } else {
-            context.startActivity(intent);
+            mContext.startActivity(intent);
         }
         return true;
     }
