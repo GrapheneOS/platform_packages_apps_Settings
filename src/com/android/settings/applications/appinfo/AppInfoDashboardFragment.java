82a63bd985eb9d4ec3467b02ec7743f17ec4c165
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-01-06 19:38:42 +0200

    add link to logcat viewer to App info screen

--- src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
+++ src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
@@ -180,6 +180,7 @@ public class AppInfoDashboardFragment extends DashboardFragment
         use(AppVersionPreferenceController.class).setParentFragment(this);
         use(AppStorageScopesPreferenceController.class).setParentFragment(this);
         use(AppContactScopesPreferenceController.class).setParentFragment(this);
+        use(AppLogcatPrefController.class).setParentFragment(this);
         use(InstantAppDomainsPreferenceController.class).setParentFragment(this);
 
         final HibernationSwitchPreferenceController appHibernationSettings =
