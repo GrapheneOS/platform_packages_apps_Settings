1baa0e2684014c750ddbb9428a977b15b9b99a8e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-01-24 19:13:22 +0200

    add toggle for eSIM support via Google's eSIM LPA package

--- src/com/android/settings/network/GoogleEuiccLpaController.java
+++ src/com/android/settings/network/GoogleEuiccLpaController.java
@@ -2,173 +2,107 @@ package com.android.settings.network;
 
 import android.Manifest;
 import android.app.AlertDialog;
-import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.os.PatternMatcher;
+import android.ext.PackageId;
 import android.os.PowerManager;
-import android.os.Process;
 import android.os.UserHandle;
 import android.permission.PermissionManager;
-import android.provider.Settings;
 
-import androidx.annotation.NonNull;
-import androidx.lifecycle.DefaultLifecycleObserver;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.preference.Preference;
-
-import com.android.internal.util.GoogleEuicc;
 import com.android.settings.R;
 import com.android.settings.ext.AbstractTogglePrefController;
+import com.android.settings.ext.ExtSettingControllerHelper;
 
-import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
+import static java.util.Objects.requireNonNull;
 
-public class GoogleEuiccLpaController extends AbstractTogglePrefController implements DefaultLifecycleObserver {
+public class GoogleEuiccLpaController extends AbstractTogglePrefController {
+    private static final String PKG_NAME = PackageId.G_EUICC_LPA_NAME;
+
     private final PackageManager packageManager;
+    private final boolean isPresent;
 
     public GoogleEuiccLpaController(Context context, String key) {
         super(context, key);
         packageManager = context.getPackageManager();
+
+        boolean isPresent = false;
+        try {
+            var ai = packageManager.getApplicationInfo(PKG_NAME, 0);
+            isPresent = ai.isSystemApp();
+        } catch (PackageManager.NameNotFoundException ignored) {}
+
+        this.isPresent = isPresent;
     }
 
     @Override
     public boolean isChecked() {
         try {
-            return packageManager.getApplicationEnabledSetting(GoogleEuicc.LPA_PKG_NAME)
-                    == COMPONENT_ENABLED_STATE_ENABLED;
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
+            return isPresent && packageManager.getApplicationInfo(PKG_NAME, 0).enabled;
+        } catch (PackageManager.NameNotFoundException e) {
             return false;
         }
     }
 
     @Override
     public int getAvailabilityStatus() {
-        if (!Process.myUserHandle().isSystem()) {
-            return DISABLED_FOR_USER;
-        }
-
-        boolean isPresent = false;
-        try {
-            var ai = packageManager.getApplicationInfo(GoogleEuicc.LPA_PKG_NAME, 0);
-            isPresent = ai.isSystemApp();
-        } catch (PackageManager.NameNotFoundException e) {
-        }
-
         if (!isPresent) {
             return UNSUPPORTED_ON_DEVICE;
         }
-
-        return GoogleEuicc.checkLpaDependencies() ? AVAILABLE : DISABLED_DEPENDENT_SETTING;
+        return ExtSettingControllerHelper.getGlobalSettingAvailability(mContext);
     }
+    
+    private void setEnabled(boolean isEnabled) {
+        String pkg = PKG_NAME;
 
-    @Override
-    public void updateState(Preference preference) {
-        super.updateState(preference);
-        preference.setEnabled(getAvailabilityStatus() == AVAILABLE);
-    }
-
-    @Override
-    public boolean setChecked(boolean isChecked) {
-        if (!isChecked) {
-            var b = new AlertDialog.Builder(mContext);
-            b.setMessage(R.string.privileged_euicc_management_restart_to_disable_dialog);
-            b.setPositiveButton(R.string.privileged_euicc_management_restart_button, (dialogInterface, btn) -> {
-                var pm = mContext.getSystemService(PowerManager.class);
-                pm.reboot(null);
-            });
-            b.show();
-            return false;
-        }
-
-        if (!GoogleEuicc.checkLpaDependencies()) {
-            // this is a race condition, toggle hasn't been grayed out yet
-            return false;
-        }
-
-        ContentResolver cr = mContext.getContentResolver();
-
-        if (Settings.Global.getInt(cr, Settings.Global.BOOT_COUNT, 0) == 1) {
-            // When BOOT_COUNT is 1, Google's LPA assumes that it was started together with Google's
-            // SetupWizard, and changes its behavior in unwanted ways.
-            //
-            // To avoid this, require a reboot, which will increment BOOT_COUNT
-
-            var b = new AlertDialog.Builder(mContext);
-            b.setMessage(R.string.privileged_euicc_management_restart_due_to_first_boot);
-            b.setPositiveButton(R.string.privileged_euicc_management_restart_button, (dialogInterface, btn) -> {
-                var pm = mContext.getSystemService(PowerManager.class);
-                pm.reboot(null);
-            });
-            b.show();
-            return false;
-        }
-
-        PermissionManager permissionManager = mContext.getSystemService(PermissionManager.class);
-
-        try {
-            String pkg = GoogleEuicc.LPA_PKG_NAME;
-
+        if (isEnabled) {
+            var permManager = mContext.getSystemService(PermissionManager.class);
             UserHandle user = mContext.getUser();
+
             String perm = Manifest.permission.CAMERA;
-            permissionManager.revokeRuntimePermission(pkg, perm, user, null);
+            permManager.revokeRuntimePermission(pkg, perm, user, null);
             // Previously, Camera permission was auto-granted with the FLAG_PERMISSION_SYSTEM_FIXED,
             // which made it unchangeable by the user.
             // Removing FLAG_PERMISSION_USER_FIXED is needed to make sure that the app is always
             // able to show a permission request dialog after being enabled
             int permFlagsToRemove = PackageManager.FLAG_PERMISSION_SYSTEM_FIXED
                     | PackageManager.FLAG_PERMISSION_USER_FIXED;
-            permissionManager.updatePermissionFlags(pkg, perm, permFlagsToRemove, 0, user);
-
-            packageManager.setApplicationEnabledSetting(pkg, COMPONENT_ENABLED_STATE_ENABLED, 0);
-            {
-                // Google's LPA expects to be enabled at boot, when ACTION_LOCKED_BOOT_COMPLETED
-                // is normally sent.
-                // Upon receiving this broadcast, it performs additional initialization that is
-                // skipped otherwise, see RestoreUiccSlotSettingsOnBootReceiver and CompleteBootService
-                var intent = new Intent(Intent.ACTION_LOCKED_BOOT_COMPLETED);
-                intent.setPackage(pkg);
-                mContext.sendBroadcast(intent);
-            }
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    private final BroadcastReceiver packageChangeListener = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (preference != null) {
-                updateState(preference);
-            }
-        }
-    };
-
-    @Override
-    public void onResume(@NonNull LifecycleOwner owner) {
-        var f = new IntentFilter();
-        f.addAction(Intent.ACTION_PACKAGE_ADDED);
-        f.addAction(Intent.ACTION_PACKAGE_CHANGED);
-        f.addAction(Intent.ACTION_PACKAGE_REMOVED);
-        f.addAction(Intent.ACTION_PACKAGE_FULLY_REMOVED);
-
-        f.addDataScheme("package");
-        for (String pkg : GoogleEuicc.getLpaDependencies()) {
-            f.addDataSchemeSpecificPart(pkg, PatternMatcher.PATTERN_LITERAL);
+            permManager.updatePermissionFlags(pkg, perm, permFlagsToRemove, 0, user);
         }
 
-        f.addDataSchemeSpecificPart(GoogleEuicc.LPA_PKG_NAME, PatternMatcher.PATTERN_LITERAL);
+        var powerManager = requireNonNull(mContext.getSystemService(PowerManager.class));
 
-        mContext.registerReceiver(packageChangeListener, f);
+        int ces = isEnabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
+                              PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+        packageManager.setApplicationEnabledSetting(pkg, ces, 0);
+
+        // Reboot is required in both cases:
+        // - OS code doesn't expect LPA to become unavailable at runtime and starts to poll for it
+        // with a high frequency
+        // - EuiccGoogle performs initialization in response to "(locked) boot completed" broadcasts
+        // - also, if current boot count is 1 (i.e. if this is the first boot), EuiccGoogle expects to
+        // be interacted with by Google's SetupWizard. Boot count will be at least 2 after reboot.
+        powerManager.reboot(null);
     }
 
     @Override
-    public void onPause(@NonNull LifecycleOwner owner) {
-        mContext.unregisterReceiver(packageChangeListener);
+    public boolean setChecked(boolean isChecked) {
+        var b = new AlertDialog.Builder(mContext);
+
+        int msg;
+        int btnText;
+        if (isChecked) {
+            msg = R.string.g_euicc_lpa_enable_dialog_msg;
+            btnText = R.string.g_euicc_lpa_restart_button;
+        } else {
+            b.setTitle(R.string.g_euicc_lpa_disable_dialog_title);
+            msg = R.string.g_euicc_lpa_disable_dialog_msg;
+            btnText = R.string.g_euicc_lpa_proceed_button;
+        }
+
+        b.setMessage(msg);
+        b.setPositiveButton(btnText, (d, btn) -> setEnabled(isChecked));
+        b.show();
+
+        return false;
     }
 }
