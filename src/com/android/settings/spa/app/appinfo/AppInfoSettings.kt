e9d7b0b887fa7c35e91bca4730a68ca0fe31c85d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-03-02 11:22:22 +0200

    spa: add Contact Scopes link to the "App info" screen

--- src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
+++ src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
@@ -143,6 +143,7 @@ private fun AppInfoSettings(packageInfoPresenter: PackageInfoPresenter) {
         AppNotificationPreference(app)
         AppPermissionPreference(app)
         AppStorageScopesPreference(app)
+        AppContactScopesPreference(app)
         AppStoragePreference(app)
         InstantAppDomainsPreference(app)
         AppDataUsagePreference(app)
