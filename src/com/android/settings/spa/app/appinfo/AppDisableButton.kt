3a537e9a8ec4d7ba9d129a26035545e97361e277
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-03-02 11:24:34 +0200

    spa: add support for disabling non-system apps

--- src/com/android/settings/spa/app/appinfo/AppDisableButton.kt
+++ src/com/android/settings/spa/app/appinfo/AppDisableButton.kt
@@ -31,6 +31,7 @@ import com.android.settingslib.spa.widget.dialog.AlertDialogButton
 import com.android.settingslib.spa.widget.dialog.rememberAlertDialogPresenter
 import com.android.settingslib.spaprivileged.framework.common.devicePolicyManager
 import com.android.settingslib.spaprivileged.framework.common.userManager
+import com.android.settingslib.spaprivileged.model.app.installed
 import com.android.settingslib.spaprivileged.model.app.isDisabledUntilUsed
 import com.android.settingslib.Utils as SettingsLibUtils
 
@@ -47,7 +48,7 @@ class AppDisableButton(
 
     @Composable
     fun getActionButton(app: ApplicationInfo): ActionButton? {
-        if (!app.isSystemApp) return null
+        if (!app.installed) return null
 
         return when {
             app.enabled && !app.isDisabledUntilUsed -> {
@@ -62,6 +63,7 @@ class AppDisableButton(
      * Gets whether a package can be disabled.
      */
     private fun ApplicationInfo.canBeDisabled(): Boolean = when {
+        !isSystemApp -> true
         // Try to prevent the user from bricking their phone by not allowing disabling of apps
         // signed with the system certificate.
         isSignedWithPlatformKey -> false
@@ -97,7 +99,9 @@ class AppDisableButton(
         ) {
             // Currently we apply the same device policy for both the uninstallation and disable
             // button.
-            if (!appButtonRepository.isUninstallBlockedByAdmin(app)) {
+            if (!app.isSystemApp) {
+                packageInfoPresenter.disable()
+            } else if (!appButtonRepository.isUninstallBlockedByAdmin(app)) {
                 dialogPresenter.open()
             }
         }
