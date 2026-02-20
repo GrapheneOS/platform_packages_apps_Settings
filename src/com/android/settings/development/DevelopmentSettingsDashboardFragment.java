54cbdea3ecb96ef5118085e3aec7b62644713b21
BasedJellyfish11 <acridstingray3@gmail.com>
2026-02-20 12:47:11 -0800

    Add a toggle to allow screenshots through FLAG_SECURE
    issue: https://github.com/GrapheneOS/os-issue-tracker/issues/664

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -620,6 +620,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new ForceEnableNotesRolePreferenceController(context));
         controllers.add(new GrammaticalGenderPreferenceController(context));
         controllers.add(new SensitiveContentProtectionPreferenceController(context));
+        controllers.add(new ForceScreenshotSecureWindowsPreferenceController(context));
         controllers.add(new ShadeDisplayAwarenessPreferenceController(context));
         controllers.add(new TextCursorBlinkRatePreferenceController(context));
 
