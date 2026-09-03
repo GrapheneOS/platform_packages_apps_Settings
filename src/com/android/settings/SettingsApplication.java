fb25e9303e97170abc932fb4049421ba57fee1f6
Daniel Micay <daniel.micay@grapheneos.org>
2026-09-03 03:22:16 -0400

    Revert "initialize display color mode to adevtool Natural default"
    This reverts commit a4d8e4b9e6396a7fe793cf9304d2bff078a5e159.

--- src/com/android/settings/SettingsApplication.java
+++ src/com/android/settings/SettingsApplication.java
@@ -106,8 +106,6 @@ public class SettingsApplication extends Application {
             ElapsedTimeUtils.assignSuwFinishedTimeStamp(getApplicationContext());
         }
 
-        com.android.settings.display.DisplayColorModeFixup.run(this);
-
         if (getUserId() == android.os.UserHandle.USER_SYSTEM) {
             com.android.settings.users.UserRestrictions.fixupPrivateSpaceRestrictions(this);
             com.android.settingslib.utils.ThreadUtils.postOnBackgroundThread(() -> {
