c60348fd981ae47f904e903ac0a44af0b10605e6
0xALUCARD <0xALUCARD@protonmail.com>
2026-09-07 19:15:13 +0200

    Update HideNavigationHandleController so it extends BoolSettingPrefController

--- src/com/android/settings/gestures/HideNavigationHandleController.java
+++ src/com/android/settings/gestures/HideNavigationHandleController.java
@@ -1,56 +1,27 @@
 /*
- * Copyright (C) 2026 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (C) 2026 GrapheneOS
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.android.settings.gestures;
 
 import android.content.Context;
-import android.provider.Settings;
+import android.ext.settings.ExtSettings;
 
-import com.android.settings.R;
-import com.android.settings.core.TogglePreferenceController;
+import com.android.settings.ext.BoolSettingPrefController;
 
 /**
  * Configures behaviour of hiding the navigation hint pill.
  */
-public class HideNavigationHandleController extends TogglePreferenceController {
+public class HideNavigationHandleController extends BoolSettingPrefController {
 
-    public HideNavigationHandleController(Context context, String key) {
-        super(context, key);
-    }
-
-    @Override
-    public boolean isChecked() {
-        return Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.HIDE_NAVIGATION_HANDLE, 0) == 1;
-    }
-
-    @Override
-    public boolean setChecked(boolean isChecked) {
-        return Settings.Secure.putInt(mContext.getContentResolver(),
-                Settings.Secure.HIDE_NAVIGATION_HANDLE, isChecked ? 1 : 0);
+    public HideNavigationHandleController(Context ctx, String key) {
+        super(ctx, key, ExtSettings.HIDE_NAVIGATION_HANDLE);
     }
 
     @Override
     public int getAvailabilityStatus() {
-        return SystemNavigationPreferenceController.isGestureAvailable(mContext) ? AVAILABLE
-                : UNSUPPORTED_ON_DEVICE;
-    }
-
-    @Override
-    public int getSliceHighlightMenuRes() {
-        return R.string.menu_key_system;
+        return SystemNavigationPreferenceController.isGestureAvailable(mContext)
+                ? super.getAvailabilityStatus() : UNSUPPORTED_ON_DEVICE;
     }
 }
